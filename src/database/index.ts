4006fec1b5a450d4aa8f84fba65241bd6a6ecf1f
Elias Junior <elijah.junnior@outlook.com>
2022-07-19 09:30:32 -0300

    fix issue in communication between the docker containers

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+// cria uma interface para tornar possível alterar um objeto Read Only
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  // copia a variável options para tornar possível editá-la
+  const newOptions = options as IOptions;
+  // adiciona ao host o nome do serviço do docker onde fica a sua base de dados
+  newOptions.host = "database_rentx";
+  // cria a conexão
+  createConnection({ ...options });
+});
