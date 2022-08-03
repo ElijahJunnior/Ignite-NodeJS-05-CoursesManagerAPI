0c0f0d55bf186fc668274d2a8683313a2491916a
Elias Jr <elijah.junnior@outlook.com>
2022-08-03 08:51:05 -0300

    username removed from users table

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -13,14 +13,12 @@ class UsersRepository implements IUsersRepository {
 
   async create({
     name,
-    username,
     email,
     password,
     driver_license,
   }: IUserCreateDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       password,
       email,
       driver_license,
