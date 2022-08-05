5433c616d2c101f22cd0c79e6e9b527ca9f405ad
Elias Jr <elijah.junnior@outlook.com>
2022-08-05 08:52:05 -0300

    Authenticate User route created

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,12 +1,57 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
 class AuthenticateUserUseCase {
-  async execute({ email, password }: IRequest) {
-    // verificar se o usuario existe
-    // verificar se a senha está correta
-    // gerar jsonwebtoken
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("E-mail or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("E-mail or password incorrect!");
+    }
+
+    const token = sign({}, "86b38cac73076ea670dc756620746b7d", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const authData: IResponse = {
+      user: {
+        email: user.email,
+        name: user.name,
+      },
+      token,
+    };
+
+    return authData;
   }
 }
+
+export { AuthenticateUserUseCase };
