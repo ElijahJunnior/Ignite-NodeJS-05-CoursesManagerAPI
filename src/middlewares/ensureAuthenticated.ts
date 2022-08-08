e8f7d3788621a9ac5e721abb5e0022e5b026a8d7
Elias Jr <elijah.junnior@outlook.com>
2022-08-08 08:27:18 -0300

    authentication middleware added to specification route

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  nxt: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing!", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "86b38cac73076ea670dc756620746b7d"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User not exists!", 401);
+    }
+
+    nxt();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+}
