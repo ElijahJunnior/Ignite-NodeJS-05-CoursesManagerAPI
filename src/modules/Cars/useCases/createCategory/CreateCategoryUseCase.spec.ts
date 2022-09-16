42fb89c3a8a303f021cfc7aa71b88eb2620f8c92
Elias Junior <elijah.junnior@outlook.com>
2022-09-16 08:59:42 -0300

    creating first automated test

--- src/modules/Cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/Cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,20 +1,16 @@
-// agrupa os teste e da um nome para o grupo
-describe("Criar categoria", () => {
-  // Cria um teste que verificar se a operação resulta no valor esperado
-  it("espero que 2 + 2 seja 4", () => {
-    const soma = 2 + 2;
-    const resultadoEsperado = 4;
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-    // Verifica se o resultado é um valor válido
-    expect(soma).toBe(resultadoEsperado);
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+let createCategoryUseCase: CreateCategoryUseCase;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
   });
 
-  // Cria um teste que verificar se a operação não resulta em um valor errado
-  it("espero que 2 + 2 não seja 5", () => {
-    const soma = 2 + 2;
-    const resultadoErrado = 5;
-
-    // Verifica se o resultado não é um valor inválido
-    expect(soma).not.toBe(resultadoErrado);
-  });
+  it("should be able to create a new category", () => {});
 });
