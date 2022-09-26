ecbf409d3ea270e21af06315fdbbc170d94355d6
Elias Junior <elijah.junnior@outlook.com>
2022-09-26 09:02:57 -0300

    new teste to createCarUseCase created

--- src/modules/Cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/Cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,13 +1,15 @@
-import { CategoriesRepositoryInMemory } from "@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
 let createCarUseCase: CreateCarUseCase;
-let repository: CategoriesRepositoryInMemory;
+let repository: ICarsRepository;
 
 describe("Create Car", () => {
   beforeEach(() => {
-    repository = new CategoriesRepositoryInMemory();
+    repository = new CarsRepositoryInMemory();
     createCarUseCase = new CreateCarUseCase(repository);
   });
 
@@ -21,5 +23,29 @@ describe("Create Car", () => {
       brand: "Brand",
       category_id: "Category",
     });
+  });
+
+  it("should not be able to create a car with exists license plate", () => {
+    expect(async () => {
+      const car1 = await createCarUseCase.execute({
+        name: "Nome do carro",
+        description: "Descrição do Carro",
+        daily_rate: 100,
+        license_plate: "ABC-12345",
+        fine_amount: 60,
+        brand: "Brand",
+        category_id: "Category",
+      });
+
+      const car2 = await createCarUseCase.execute({
+        name: "Nome do carro",
+        description: "Descrição do Carro",
+        daily_rate: 100,
+        license_plate: "ABC-12345",
+        fine_amount: 60,
+        brand: "Brand",
+        category_id: "Category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
