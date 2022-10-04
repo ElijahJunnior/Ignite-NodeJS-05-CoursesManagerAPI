acaee3e04987f4143462ad194e14a6401c24a078
Elias Junior <elijah.junnior@outlook.com>
2022-10-04 13:10:15 -0300

    creation of the upload car image use case

---
+++ src/modules/Cars/useCases/uploadCarsImage/UploadCarImageUseCase.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject("CarsImagesRepository")
+    private carsImagesRepository: ICarsImagesRepository
+  ) {}
+
+  async execute({ car_id, images_name }: IRequest) {
+    //
+  }
+}
+
+export { UploadCarImageUseCase };
