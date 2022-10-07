f2e212e0cd7bf75f7e694e8b50debbb1bbe7d445
Elias Junior <elijah.junnior@outlook.com>
2022-10-07 08:41:32 -0300

    fix erro on the creation rentals database migrations

--- src/shared/infra/typeorm/migrations/1664967675901-creation-of-the-rentals-table.ts
+++ src/shared/infra/typeorm/migrations/1664967675901-creation-of-the-rentals-table.ts
@@ -29,6 +29,7 @@ export class creationOfTheRentalsTable1664967675901
           {
             name: "end_date",
             type: "timestamp",
+            isNullable: true,
           },
           {
             name: "expected_return_date",
@@ -37,6 +38,7 @@ export class creationOfTheRentalsTable1664967675901
           {
             name: "total",
             type: "numeric",
+            isNullable: true,
           },
           {
             name: "created_at",
