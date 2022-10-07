364f8c869538199dcbf631c114d83a5b6ca2746d
Elias Junior <elijah.junnior@outlook.com>
2022-10-07 08:27:39 -0300

    creation of the create rental route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carRouter } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { rentalsRoutes } from "./rentals.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { userRoutes } from "./users.routes";
 
@@ -13,5 +14,6 @@ router.use("/cars", carRouter);
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", userRoutes);
+router.use("/rentals", rentalsRoutes);
 
 export { router };
