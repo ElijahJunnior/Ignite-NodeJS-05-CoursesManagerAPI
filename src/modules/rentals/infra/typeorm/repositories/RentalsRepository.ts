798dc13775d2f7ab030eb88dd8339c2482681643
Elias Junior <elijah.junnior@outlook.com>
2022-10-25 08:38:35 -0300

    creation of the Devolution Rental Controller

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -14,13 +14,19 @@ class RentalsRepository implements IRentalsRepository {
 
   async create({
     car_id,
-    user_id,
     expected_return_date,
+    end_date,
+    id,
+    total,
+    user_id,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
-      user_id,
       expected_return_date,
+      end_date,
+      id,
+      total,
+      user_id,
     });
 
     await this.repository.save(rental);
@@ -33,12 +39,22 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id });
+    const openByCar = await this.repository.findOne({
+      where: {
+        car_id,
+        end_date: null,
+      },
+    });
     return openByCar;
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({ user_id });
+    const openByUser = await this.repository.findOne({
+      where: {
+        user_id,
+        end_date: null,
+      },
+    });
     return openByUser;
   }
 }
