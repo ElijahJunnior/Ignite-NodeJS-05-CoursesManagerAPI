d3c6d5161ac6d88ab90c2f48eb322e0a01dd20e3
Elias Junior <elijah.junnior@outlook.com>
2022-10-28 13:22:23 -0300

    adding creation of the refresh token code in authenticate user use case

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "86b38cac73076ea670dc756620746b7d",
+  expires_in_token: "15m",
+  secret_refresh_token: "7ee27c484f387ff4bba373dd5a8cbab1",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
