0f99188b7ba295156c75cfe54f42de6102ef80f4
Elias Junior <elijah.junnior@outlook.com>
2022-10-31 08:55:11 -0300

    creation of the Refresh Token Controller

--- src/modules/accounts/useCases/refreshTöken/RefreshTokenController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-class RefreshTokenController {
-  async handle(req: Request, res: Response): Promise<Response> {
-    return res.send();
-  }
-}
-
-export { RefreshTokenController };
