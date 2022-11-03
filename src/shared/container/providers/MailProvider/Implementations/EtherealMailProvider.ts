2068a6c60bc9862c5a28aa978f1a978783db7de1
Elias Junior <elijah.junnior@outlook.com>
2022-11-03 08:54:15 -0300

    changing the send forgot password mail use case to use handlebars with template compiler

--- src/shared/container/providers/MailProvider/Implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/Implementations/EtherealMailProvider.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import handlebars from "handlebars";
 import nodemailer, { Transporter } from "nodemailer";
 
 import { IMailProvider } from "../IMailProvider";
@@ -5,6 +7,7 @@ import { IMailProvider } from "../IMailProvider";
 class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
   constructor() {
+    // função que cria o cliente do nodemailer responsável pelo envio dos emails
     nodemailer
       .createTestAccount()
       .then((account) => {
@@ -20,17 +23,35 @@ class EtherealMailProvider implements IMailProvider {
 
         this.client = transporter;
       })
-      .catch((err) => console.error(err));
+      .catch((err) =>
+        console.error(
+          "*** Error in Function nodemailer.createTestAccount *** ",
+          err
+        )
+      );
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
-    console.log("aqui");
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    // efetua a leitura do arquivo de template e grava em uma variável string
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    // cria uma função, usando o handlebars, para compilar o email
+    const templateParse = handlebars.compile(templateFileContent);
+
+    // compila o template substituindo as chaves pelas variáveis
+    const templateHTML = templateParse(variables);
+
+    // função que uso o cliente do nodemailer para enviar o email
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreplay@rentx.com.br>",
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log("Message sent: %s", message.messageID);
