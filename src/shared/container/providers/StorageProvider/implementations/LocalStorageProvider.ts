923c2d100739f1f44baaf2f9498b4f24ea7826a1
Elias Junior <elijah.junnior@outlook.com>
2022-11-16 08:17:26 -0300

    creation of the provider to save file in AWS S3 storage

--- src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -7,16 +7,18 @@ import { IStorageProvider } from "../IStorageProvider";
 
 class LocalStorageProvider implements IStorageProvider {
   async save(file: string, folder: string): Promise<string> {
+    // resolve o path absoluto atual do arquivo
+    const origin_path = resolve(upload.tmpFolder, file);
+
+    // resolve o novo path absoluto do arquivo
+    const destination_path = resolve(upload.tmpFolder, folder, file);
+
     // move o arquivo de um local para outro
-    await fs.promises.rename(
-      // resolve o path absoluto atual do arquivo
-      resolve(upload.tmpFolder, file),
-      // resolve o novo path absoluto do arquivo
-      resolve(`${upload.tmpFolder}/${folder}`)
-    );
+    await fs.promises.rename(origin_path, destination_path);
 
     return file;
   }
+
   async delete(file: string, folder: string): Promise<void> {
     // Resolvendo o path absoluto do arquivo
     const file_name = resolve(`${upload.tmpFolder}/${folder}`, file);
@@ -27,6 +29,7 @@ class LocalStorageProvider implements IStorageProvider {
     } catch {
       return;
     }
+
     // apaga o arquivo
     await fs.promises.unlink(file_name);
   }
