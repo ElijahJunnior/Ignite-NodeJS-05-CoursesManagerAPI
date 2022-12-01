ba10aa7390060bd8dd9dd225db0d28a23761bb0b
Elias Junior <elijah.junnior@outlook.com>
2022-12-01 07:36:05 -0300

    change database connection to use application without docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 // Altera o nome da base informado no ormconfig
-export default async (host = "database_rentx"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
