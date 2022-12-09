e7e5826409170e822d96cccbfda3733bc30c6cd6
Elias Junior <elijah.junnior@outlook.com>
2022-12-09 09:09:37 -0300

    adding rate mimliter flexible to project

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,7 @@
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import redis from "redis";
+
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+// });
